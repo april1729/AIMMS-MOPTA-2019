f70b87aa501bda9771acdff9dbfd55fc82920b80
april1729 <sagana@rpi.edu>
2019-04-06 20:31:40 -0400

    First commit

---
+++ clustered_slicing_hueristic.m
@@ -0,0 +1,69 @@
+function [z,x,y, x0] = clustered_slicing_hueristic(w,h,W,H, cuts)
+
+m=length(cuts)-1;
+n=length(w);
+
+for i=1:m
+    c{i}=find( (cuts(i) < h).*(cuts(i+1)>=h));
+    gh(i)=max(h(c{i}));
+    [z,v(i)]=solveKnapsack(w(c{i}),w(c{i}), W);
+    zg{i}=c{i}(find(z>0.5));
+end
+
+[slices]=solveKnapsack((-v.*gh)', gh', H);
+
+z=[];
+x=[];
+y=[];
+
+yk=0;
+for i=find(slices>0.5)'
+    xk=0;
+    z=[z;zg{i}];
+    for j=zg{i}'
+        x=[x;xk];
+        y=[y;yk];
+        xk=xk+w(j);
+    end
+    yk=yk+gh(i);
+end
+draw_rectangles(z,x,y,w(z),h(z),W,H)
+
+x0=zeros(3*n, 1);
+
+x0(z)=1;
+x0(z+n)=x;
+x0(z+2*n)=y;
+
+dy=zeros(n,n);
+dx=zeros(n,n);
+
+for i=1:length(z)
+    for j=1:length(z)
+       if z(i)> z(j)
+           %fprintf("i %i, j %i \n",z(i), z(j))
+            if x(i)+w(z(i)) <= x(j)% is i to the left of j?
+                dx(z(i),z(j))=1;
+            elseif x(j)+w(z(j)) <= x(i)% is j to the left of i?
+                dx(z(j),z(i))=1;
+            elseif y(i) +h(z(i)) <= y(j)% is i below j?
+                dy(z(i),z(j))=1;
+            elseif y(j)+h(z(j)) <=y(i)% is j below i?
+                dy(z(j),z(i))=1;
+            end
+        end
+    end
+end
+x0=[x0;dx(:);dy(:)];
+    function [z,obj]=solveKnapsack(v,w, C)
+        %solves max z_i v_i
+        %       s.t. sum z_i w_i \leq C
+        
+        A=w';
+        b=C;
+        f=-v';
+        intcon=1:length(v);
+        z = intlinprog(f,intcon,A,b,[],[],zeros(size(f')),ones(size(f')));
+        obj= f*z;
+    end
+end
---
+++ global_solver_milp.m
@@ -0,0 +1,125 @@
+function [z,x,y] = global_solver_milp(l,w,L,W, x0)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+n=length(l);
+
+A=sparse(5*n^2+1,3*n+2*n^2);
+b=zeros(5*n^2+1,1);
+intcon=[1:n, 3*n+1:3*n+2*n^2];
+
+for i=1:n
+    for j=1:n
+        if i~=j
+            
+            k=get_constraint_number(A);
+            A(k,get_index_x(1,i))=1;
+            A(k,get_index_x(1,j))=-1;
+            A(k,get_index_c(1,i,j))=L;
+            b(k)=L-l(i);
+            
+            k=get_constraint_number(A);
+            A(k,get_index_x(2,i))=1;
+            A(k,get_index_x(2,j))=-1;
+            A(k,get_index_c(2,i,j))=W;
+            b(k)=W-w(i);
+            
+            if i>j
+                k=get_constraint_number(A);
+                A(k, i)=1;
+                A(k,j)=1;
+                A(k,get_index_c(1,i,j))=-1;
+                A(k,get_index_c(1,j,i))=-1;
+                A(k,get_index_c(2,i,j))=-1;
+                A(k,get_index_c(2,j,i))=-1;
+                b(k)=1;
+            end
+        end
+    end
+end
+
+% Knapsack constraint
+k=get_constraint_number(A);
+A(k, 1:n)=1*(l.*w);
+b(k)=L*W;
+
+% we need to take at least x items, elimates a ton of bad points
+k=get_constraint_number(A);
+A(k, 1:n)=-1;
+b(k)=-3;
+
+% if we arent using a particular object, dont worry about the placement
+% constraints for it 
+
+for i=1:n
+    for j=1:n
+        
+    k=get_constraint_number(A);
+    A(k, i)=-1;
+    A(k, get_index_c(1,i,j))=1;
+    b(k)=0;
+    
+    k=get_constraint_number(A);
+    A(k, i)=-1;
+    A(k, get_index_c(1,j,i))=1;
+    b(k)=0;
+    
+    k=get_constraint_number(A);
+    A(k, i)=-1;
+    A(k, get_index_c(2,i,j))=1;
+    b(k)=0;
+    
+    k=get_constraint_number(A);
+    A(k, i)=-1;
+    A(k, get_index_c(2,j,i))=1;
+    b(k)=0;
+
+    end
+end
+
+
+f=zeros(3*n+2*n^2,1);
+f(1:n)=-1*(l.*w);
+
+lb=zeros(size(f));
+ub=ones(size(f));
+ub(n+1:2*n)=L-l;
+ub(2*n+1:3*n)=W-w;
+
+soln = intlinprog(f,intcon,A,b,[],[],lb,ub);
+
+
+
+z=find(soln(1:n)>0.5);
+x=soln((n+1):2*n);
+y=soln((2*n+1):3*n);
+cx=reshape(soln(3*n+1:3*n+n^2), [n,n]);
+cy=reshape(soln(3*n+n^2+1:3*n+2*n^2), [n,n]);
+
+draw_rectangles(z,x(z),y(z),l(z),w(z), L, W)
+
+
+    function k=get_index_c(x_or_y,i,j)
+        C=zeros(n,n);
+        C(i,j)=1;
+        if x_or_y==1 % get cx index
+            k=3*n+find(C(:)==1);
+        else %get cy index
+            k=3*n+n^2+find(C(:)==1);
+        end
+    end
+
+    function k=get_index_x(x_or_y,i)
+        
+        if x_or_y==1 % get x index
+            k=n+i;
+        else %get y index
+            k=2*n+i;
+        end
+    end
+
+    function k=get_constraint_number(A)
+        k=min(find(sum(abs(A), 2)==0));
+    end 
+
+end
